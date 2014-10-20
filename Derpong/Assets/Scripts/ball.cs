34b988fd6bcab9b1ade88f75a9500525fea94e44
ZeldaTechie <jereyes4@broncs.utpa.edu>
2014-10-20 15:23:48 -0500

    Placed the project inside

---
+++ Derpong/Assets/Scripts/ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	public GameObject spawn;
+	public float speed = 10.0f;
+
+	void move()
+	{
+		rigidbody2D.velocity = transform.up.normalized * speed;
+	}
+	void resetBall()
+	{
+		transform.position = spawn.transform.position;
+		transform.Rotate (Vector3.forward * Random.Range(0,360));
+		move();
+	}
+
+	// Use this for initialization
+	void Start () {
+		resetBall ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("left"))
+			resetBall ();
+	}
+}
---
+++ Derpong/Assets/Scripts/paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paddle : MonoBehaviour {
+
+	void move()
+	{
+		transform.Translate (Input.GetAxis("Horizontal") * 10* Time.deltaTime, 0 , 0);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+}
